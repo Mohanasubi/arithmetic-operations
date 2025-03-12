a5d2b352714c775bc02c98e8504643bfa425e251
Mohanasubi <mohanasubivenkatachalapathi>
2025-03-12 13:58:06 +0530

    ADD - Include the functionality to find the difference of two numbers

--- solution.js
+++ solution.js
@@ -4,3 +4,12 @@ let secondInput = 10
 function calculateSum(firstNumber,secondNumber){
    return firstNumber+ secondNumber
 }
+
+function calculateDifference(firstNumber,secondNumber){
+    return firstNumber - secondNumber
+ }
+
+console.log(calculateDifference(firstInput,secondInput))
+console.log(calculateSum(firstInput,secondInput))
+
+
